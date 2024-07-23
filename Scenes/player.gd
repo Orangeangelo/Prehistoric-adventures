1c38275f7b234d5c18fb0fa937847c5a729c36f9
Orangeangelo <evasan46@kingshigh.school.nz>
2024-07-23 14:43:21 +1200

    1.3
    Animation

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -16,6 +16,36 @@ var anim_state = state.IDLE
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func update_state():
+	if anim_state == state.HURT:
+		return
+	if is_on_floor():
+		if velocity == Vector2.ZERO:
+			anim_state = state.IDLE
+		elif velocity.x !=0:
+			anim_state = state.RUNNING
+	else:
+		if velocity.y < 0:
+			anim_state = state.JUMPUP
+		else:
+			anim_state = state.JUMPDOWN
+
+func update_animation(direction):
+	if direction > 0:
+		animator.flip_h = false
+	elif direction < 0:
+		animator.flip_h = true
+	match anim_state:
+		state.IDLE:
+			animation_player.play("idle")
+		state.RUNNING:
+			animation_player.play("run")
+		state.JUMPUP:
+			animation_player.play("jump_up")
+		state.JUMPDOWN:
+			animation_player.play("jump_down")
+		state.HURT:
+			animation_player.play("hurt")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -34,5 +64,6 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
-
+	update_state()
+	update_animation(direction)
 	move_and_slide()
