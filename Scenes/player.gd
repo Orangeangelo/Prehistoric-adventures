0e64fbfeb126654d061e3b528708c7e49b15b558
Orangeangelo <evasan46@kingshigh.school.nz>
2024-08-05 10:57:13 +1200

    1.4
    added tilemap and layers also exported it.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -60,9 +60,9 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = move_toward(velocity.x,direction * speed,acceleration)
+		velocity.x = move_toward(velocity.x,direction*speed,acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, acceleration)
 		
 	update_state()
 	update_animation(direction)
